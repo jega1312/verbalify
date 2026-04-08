fce387cf0207975c67085fcdb82f9c3c05c0bdde
Jegathiswaran Thiaghu <thiagu1312@gmail.com>
2026-04-08 19:57:24 +0800

    Footer Section Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,245 @@
-import React from 'react'
+import { SlSocialLinkedin, SlEnvolope, SlGlobe } from "react-icons/sl";
+import { FiGithub } from "react-icons/fi";
+import { motion, useInView } from "motion/react";
+import { useRef } from "react";
+import { NavLink } from "react-router-dom";
+motion;
+
+const productLinks = [
+  { id: 1, label: "Features", to: "/features" },
+  { id: 2, label: "Pricing", to: "/pricing" },
+];
+
+const companyLinks = [
+  { id: 1, label: "About", to: "/about" },
+  { id: 2, label: "Contact", to: "/contact" },
+];
+
+const socialLinks = [
+  {
+    id: 1,
+    href: "https://jega1312.github.io/portfolio/",
+    icon: <SlGlobe size={20} />,
+    target: "_blank",
+    rel: "noopener noreferrer",
+  },
+  {
+    id: 2,
+    href: "https://github.com/jega1312",
+    icon: <FiGithub size={20} />,
+    target: "_blank",
+    rel: "noopener noreferrer",
+  },
+  {
+    id: 3,
+    href: "https://www.linkedin.com/in/jegathiswaran-thiaghu/",
+    icon: <SlSocialLinkedin size={20} />,
+    target: "_blank",
+    rel: "noopener noreferrer",
+  },
+  {
+    id: 4,
+    href: "mailto:jegathiaghu@gmail.com?subject=Hello%20from%20ResumeIQ",
+    icon: <SlEnvolope size={20} />,
+  },
+];
+
+// Mapped Elements Animation
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
+};
+
+const socialContainerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const socialItemVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.8, ease: "easeOut" },
+  },
+};
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  // Start Animation When Visible
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
 
-export default Footer
+  return (
+    <footer className="w-full border-t border-gray-200 bg-white flex flex-col gap-10 items-center">
+      <div className="w-[90%] mx-auto py-3">
+        {/* Link Container */}
+        <div className="grid grid-cols-1 gap-7 lg:gap-0 lg:grid-cols-4 place-items-start py-14">
+          {/* Logo Container */}
+          <div className="flex flex-col gap-3 items-start">
+            <NavLink
+              className="flex gap-2 items-center hover:cursor-pointer"
+              to="/"
+            >
+              <motion.span
+                ref={ref}
+                initial={{ opacity: 0, y: 40 }}
+                animate={isInView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
+                className="text-2xl dm-bold text-green-500 w-full"
+              >
+                Verbalify
+              </motion.span>
+            </NavLink>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={isInView ? { opacity: 1 } : {}}
+              transition={{ duration: 0.6, ease: "easeOut", delay: 0.3 }}
+              className="text-sm text-black sora-medium text-balance"
+            >
+              Language learning, beautifully simplified.
+            </motion.p>
+          </div>
+          {/* Product Links Container */}
+          <motion.div
+            ref={ref}
+            variants={containerVariants}
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            className="flex flex-col items-start gap-3"
+          >
+            <motion.h3
+              ref={ref}
+              initial={{ opacity: 0, y: 40 }}
+              animate={isInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
+              className="sora-medium text-lg text-black"
+            >
+              Product
+            </motion.h3>
+            <motion.ul className="flex flex-col items-start gap-2">
+              {productLinks.map((product) => (
+                <motion.li
+                  key={product.id}
+                  variants={itemVariants}
+                  className="group"
+                >
+                  <NavLink
+                    to={product.to}
+                    className={({ isActive }) =>
+                      `text-sm sora-light hover:text-green-500 transition duration-300 ease-in-out hover:cursor-pointer ${isActive ? "text-green-500" : "text-gray-500"}`
+                    }
+                  >
+                    {product.label}
+                  </NavLink>
+                </motion.li>
+              ))}
+            </motion.ul>
+          </motion.div>
+
+          {/* Company Links Container */}
+          <motion.div
+            ref={ref}
+            variants={containerVariants}
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            className="flex flex-col items-start gap-3"
+          >
+            <motion.h3
+              ref={ref}
+              initial={{ opacity: 0, y: 40 }}
+              animate={isInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
+              className="sora-medium text-lg text-black"
+            >
+              Company
+            </motion.h3>
+            <motion.ul className="flex flex-col items-start gap-2">
+              {companyLinks.map((company) => (
+                <motion.li
+                  key={company.id}
+                  variants={itemVariants}
+                  className="group"
+                >
+                  <NavLink
+                    to={company.to}
+                    className={({ isActive }) =>
+                      `text-sm sora-light hover:text-green-500 transition duration-300 ease-in-out hover:cursor-pointer ${isActive ? "text-green-500" : "text-gray-500"}`
+                    }
+                  >
+                    {company.label}
+                  </NavLink>
+                </motion.li>
+              ))}
+            </motion.ul>
+          </motion.div>
+
+          {/* Social Buttons */}
+          <div className="flex flex-col gap-3 items-start">
+            <motion.h3
+              ref={ref}
+              initial={{ opacity: 0, y: 40 }}
+              animate={isInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
+              className="sora-medium text-lg text-black"
+            >
+              Follow Us
+            </motion.h3>
+            <motion.div
+              ref={ref}
+              variants={socialContainerVariants}
+              initial="hidden"
+              animate={isInView ? "visible" : "hidden"}
+              className="flex gap-7"
+            >
+              {socialLinks.map((social) => (
+                <motion.a
+                  key={social.id}
+                  href={social.href}
+                  target={social.target}
+                  rel={social.rel}
+                  variants={socialItemVariants}
+                  className="bg-transparent rounded-lg flex justify-center items-center transition duration-300 ease-in-out"
+                >
+                  <span className="text-black hover:text-green-500 transition duration-300 ease-in-out">
+                    {social.icon}
+                  </span>
+                </motion.a>
+              ))}
+            </motion.div>
+          </div>
+        </div>
+
+        {/* Footer Below */}
+        <div className="w-full border-t border-gray-200 flex flex-col gap-7 lg:gap-0 lg:flex-row justify-center items-center py-10">
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={isInView ? { opacity: 1 } : {}}
+            transition={{ duration: 0.6, ease: "easeOut", delay: 0.1 }}
+            className="text-sm text-black sora-regular"
+          >
+            © 2026 Verbalify. All rights reserved.
+          </motion.p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,93 +54,57 @@ const Navbar = () => {
   }, [menuOpen]);
 
   return (
-    <>
-      <nav className="fixed w-full top-0 flex justify-between flex-wrap items-center py-6 px-8 lg:px-10  z-50 border-b transition duration-300 ease-in-out bg-white border-gray-200">
-        {/* Logo Container */}
-        <motion.div
-          initial={{ opacity: 0, x: -30 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+    <nav className="fixed w-full top-0 flex justify-between flex-wrap items-center py-6 px-8 lg:px-10  z-50 border-b transition duration-300 ease-in-out bg-white border-gray-200">
+      {/* Logo Container */}
+      <motion.div
+        initial={{ opacity: 0, x: -30 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+      >
+        <NavLink
+          to="/"
+          className="flex gap-2 items-center hover:cursor-pointer"
         >
-          <NavLink
-            to="/"
-            className="flex gap-2 items-center hover:cursor-pointer"
-          >
-            <span className="text-2xl dm-extrabold text-green-500 w-full">
-              Verbalify
-            </span>
-          </NavLink>
-        </motion.div>
+          <span className="text-2xl dm-extrabold text-green-500 w-full">
+            Verbalify
+          </span>
+        </NavLink>
+      </motion.div>
 
-        {/* Hamburger Menu */}
-        <button
-          type="button"
-          onClick={toggleOpen}
-          className="relative flex items-center lg:hidden flex-col gap-2 h-5 w-8"
-        >
-          <span
-            className={`absolute h-0.75 w-8 bg-green-500 rounded-full transition duration-300 ease-in-out  ${menuOpen ? "rotate-45 top-2" : "top-0"}`}
-          ></span>
-          <span
-            className={`absolute top-2 h-0.75 w-8 bg-green-500 rounded-full transition duration-300 ease-in-out  ${menuOpen ? "opacity-0" : ""}`}
-          ></span>
-          <span
-            className={`absolute h-0.75 w-8 bg-green-500 rounded-full  transition duration-300 ease-in-out ${menuOpen ? "-rotate-45 top-2" : "top-4"}`}
-          ></span>
-        </button>
+      {/* Hamburger Menu */}
+      <button
+        type="button"
+        onClick={toggleOpen}
+        className="relative flex items-center lg:hidden flex-col gap-2 h-5 w-8"
+      >
+        <span
+          className={`absolute h-0.75 w-8 bg-green-500 rounded-full transition duration-300 ease-in-out  ${menuOpen ? "rotate-45 top-2" : "top-0"}`}
+        ></span>
+        <span
+          className={`absolute top-2 h-0.75 w-8 bg-green-500 rounded-full transition duration-300 ease-in-out  ${menuOpen ? "opacity-0" : ""}`}
+        ></span>
+        <span
+          className={`absolute h-0.75 w-8 bg-green-500 rounded-full  transition duration-300 ease-in-out ${menuOpen ? "-rotate-45 top-2" : "top-4"}`}
+        ></span>
+      </button>
 
-        {/* Menu Mobile Dropdown */}
-        <div
-          className={`h-screen ${menuOpen ? "flex" : "hidden"} lg:hidden w-full flex-col items-center -mt-20 justify-center gap-7 transition duration-300 ease-in-out`}
-        >
-          <ul className="flex flex-col items-center justify-center gap-5 list-none w-full">
-            {navLinks.map((link) => (
-              <li key={link.id}>
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={hasOpened ? { opacity: 1 } : { opacity: 0 }}
-                  transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
-                  onClick={() => setMenuOpen(false)}
-                >
-                  <NavLink
-                    to={link.to}
-                    className={({ isActive }) =>
-                      `sora-regular text-3xl relative hover:text-green-500 transition duration-300 ease-in-out ${isActive ? "text-green-500" : "text-black"}`
-                    }
-                  >
-                    {link.label}
-                  </NavLink>
-                </motion.div>
-              </li>
-            ))}
-          </ul>
-
-          {/* Get Started Mobile Button */}
-          <motion.div
-            type="button"
-            initial={{ opacity: 0, y: 20 }}
-            animate={hasOpened ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-            transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
-            onClick={() => setMenuOpen(false)}
-            className="py-2.5 px-4 sora-regular bg-green-500 text-white text-lg rounded-sm transition duration-300 ease-in-out relative hover:bg-green-500/80 hover:cursor-pointer"
-          >
-            <NavLink to="/contact">Get Started</NavLink>
-          </motion.div>
-        </div>
-
-        {/* Desktop Links */}
-        <ul className="hidden lg:flex items-center gap-4 list-none">
+      {/* Menu Mobile Dropdown */}
+      <div
+        className={`h-screen ${menuOpen ? "flex" : "hidden"} lg:hidden w-full flex-col items-center -mt-20 justify-center gap-7 transition duration-300 ease-in-out`}
+      >
+        <ul className="flex flex-col items-center justify-center gap-5 list-none w-full">
           {navLinks.map((link) => (
             <li key={link.id}>
               <motion.div
                 initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
+                animate={hasOpened ? { opacity: 1 } : { opacity: 0 }}
                 transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
+                onClick={() => setMenuOpen(false)}
               >
                 <NavLink
                   to={link.to}
                   className={({ isActive }) =>
-                    `sora-regular text-lg relative hover:text-green-500 transition duration-300 ease-in-out ${isActive ? "text-green-500" : "text-black"}`
+                    `sora-regular text-3xl relative hover:text-green-500 transition duration-300 ease-in-out ${isActive ? "text-green-500" : "text-black"}`
                   }
                 >
                   {link.label}
@@ -150,18 +114,52 @@ const Navbar = () => {
           ))}
         </ul>
 
-        {/* Get Started Desktop Button */}
+        {/* Get Started Mobile Button */}
         <motion.div
           type="button"
-          initial={{ opacity: 0, x: 30 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="hidden lg:block py-2 px-4 sora-regular bg-green-500 text-white rounded-sm transition duration-300 ease-in-out relative hover:bg-green-500/80 hover:cursor-pointer"
+          initial={{ opacity: 0, y: 20 }}
+          animate={hasOpened ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
+          onClick={() => setMenuOpen(false)}
+          className="py-2.5 px-4 dm-medium bg-green-500 text-white text-lg rounded-sm transition duration-300 ease-in-out relative hover:bg-green-500/85 hover:text-black hover:shadow-md hover:cursor-pointer"
         >
           <NavLink to="/contact">Get Started</NavLink>
         </motion.div>
-      </nav>
-    </>
+      </div>
+
+      {/* Desktop Links */}
+      <ul className="hidden lg:flex items-center gap-4 list-none">
+        {navLinks.map((link) => (
+          <li key={link.id}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
+            >
+              <NavLink
+                to={link.to}
+                className={({ isActive }) =>
+                  `sora-regular text-lg relative hover:text-green-500 transition duration-300 ease-in-out ${isActive ? "text-green-500" : "text-black"}`
+                }
+              >
+                {link.label}
+              </NavLink>
+            </motion.div>
+          </li>
+        ))}
+      </ul>
+
+      {/* Get Started Desktop Button */}
+      <motion.div
+        type="button"
+        initial={{ opacity: 0, x: 30 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        className="hidden lg:block py-2 px-4 dm-medium bg-green-500 text-white rounded-sm transition duration-300 ease-in-out relative hover:bg-green-500/85 hover:text-black hover:shadow-md hover:cursor-pointer"
+      >
+        <NavLink to="/contact">Get Started</NavLink>
+      </motion.div>
+    </nav>
   );
 };
 
